ce839eb171ba28f00562e546f14dea6a07a74fae
dudufox <99772148+dudufox@users.noreply.github.com>
2025-09-08 23:24:20 +0000

    Adicionado o cálculo Densidade e Per Capita da Cidade

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
     int main () {
@@ -15,7 +14,7 @@
         char  Cidade1 [20];
         char  Cidade2 [20];
         int   Populacao1, Populacao2, PontoTuristico1, PontoTuristico2;
-        float Area1, Area2, Pib1, Pib2 ;
+        float Area1, Area2, Pib1, Pib2, PerCidade1, PerCidade2, DensiCidade1, DensiCidade2 ;
         
                 // Irá solicitar as informações da primeira carta
             printf("\nInsira dos dados da primeira carta");
@@ -40,14 +39,22 @@
             printf("Digite a quantidade de pontos turísticos:");
             scanf("%d", &PontoTuristico1);
 
-              // Irá solicitar as inforfmações da segunda carta
+                //Cáuculo Densidade Cidade Carta 01
+
+                DensiCidade1 = Populacao1 / Area1;
+
+                //Cáuculo Pib Per Capita Cidade Carta 01
+
+                PerCidade1 = Pib1 / Populacao1; 
+
+                // Irá solicitar as inforfmações da segunda carta
 
             printf("\nInsira dos dados da segunda carta");
             printf("\nInsira uma letra de 'A' a 'H', representando um estado: ");
             scanf(" %c", &Estado2);
 
             printf("\nInsira o código da carta sendo iniciado pela letra do estado seguida de um númedo de '01'a '04': ");
-            scanf(" %s", &CodigoCarta2);
+            scanf("%s", &CodigoCarta2);
 
             printf("Digite o nome da cidade: ");
             scanf("%s", &Cidade2);
@@ -64,6 +71,14 @@
             printf("Digite a quantidade de pontos turísticos:");
             scanf("%d", &PontoTuristico2);
 
+                //Cáuculo Densidade Cidade Carta 02
+
+                DensiCidade2 = Populacao2 / Area2;
+
+                //Cáuculo Pib Per Capita Cidade Carta 02
+
+                PerCidade2 = Pib2 / Populacao2; 
+
                 // Irá exibir as informações da primeira carta
 
             printf("\nCarta 1:\n");
@@ -74,6 +89,9 @@
             printf("Área: %.2f km²\n", Area1);
             printf("Pib: %.2f bilhões de reais\n", Pib1);
             printf("Número de Pontos Turísticos: %d\n", PontoTuristico1);
+            printf("Densidade Populacional: %.2f hab/Km²\n", DensiCidade1);
+            printf("PIB Per Capita: %f\n", PerCidade1);
+            
 
              // Irá exibir as informações da segunda carta
 
@@ -84,7 +102,11 @@
             printf("População: %d\n", Populacao2);
             printf("Área: %.2f km²\n", Area2);
             printf("Pib: %.2f bilhões de reais\n", Pib2);
-            printf("Número de Pontos Turísticos: %d\n\n", PontoTuristico2);
+            printf("Número de Pontos Turísticos: %d\n", PontoTuristico2);
+            printf("Densidade Populacional: %.2f hab/Km²\n", DensiCidade2);
+            printf("PIB Per Capita: %f\n\n", PerCidade2);
+
+
 
 
 
